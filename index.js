95e3a6563e6da72cf606193da417687247dccf34
laura-paullette <laura.paullette@student.moringaschool.com>
2023-03-10 16:27:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+// Code your solutions in this fil
+
+
+function writeCards(name, event) {
+    const myArray = [];
+    for (let i = 0; i < name.length; i++) {
+        const message = `Thank you, ${name[i]}, for the wonderful ${event} gift!`;
+        myArray.push(message);
+    
+    }
+    return myArray;
+}
+
+function countDown(num) {
+    do {
+        console.log(num);
+        num--;
+    }
+    while (num >= 0);
+}
